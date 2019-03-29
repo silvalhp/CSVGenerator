4b1e7277e4df18c3abff92e522e6d086ffe68572
Luiz Henrique Pereira da Silva <luiz.silva@eldorado.org.br>
2019-03-29 08:54:24 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+const os = require("os");
+const fs = require("fs");
+
+function queryMetric() {
+
+}
+
+function createMetric() {
+
+}
+
+function convertMetricToCSV(metric) {
+
+}
+
+function addMetricsToMetricList(metric) {
+
+}
+
+function createResultsFolder() {
+	
+}
